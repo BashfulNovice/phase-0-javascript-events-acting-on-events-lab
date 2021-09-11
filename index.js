eff6aa420da0a8d6bb5cf58e6133fda3014b7ee5
Todd Krieger <twkriege@mtu.edu>
2021-09-11 16:50:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+//Code for moveing dodger left provided by lab.
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+//additional function to move right
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+
+document.addEventListener("keydown", function (e) {
+     if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    //additional else if to not use another event listener.
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
