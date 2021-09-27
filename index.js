e51be4b8c927c8747ada11a5d73f99241e832a4a
Todd Krieger <twkriege@mtu.edu>
2021-09-27 17:59:13 -0400

    completed assignment again, improved solution, fixed bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
 
-//Code for moveing dodger left provided by lab.
+//Code for moveing dodger left provided by lab.-----------------------------------------
 
 const dodger = document.getElementById("dodger");
 
@@ -12,12 +12,12 @@ function moveDodgerLeft() {
       dodger.style.left = `${left - 1}px`;
     }
   }
-//additional function to move right
+//additional function to move right -------------------------------------------------------
   function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
   
-    if (left > 0) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
